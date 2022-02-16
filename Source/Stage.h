2305a4563957002ba46e870ca940962d77c68070
dorafumi <93898530+dorafumi@users.noreply.github.com>
2022-02-16 15:35:01 +0900

    papapapapa

---
+++ Source/Stage.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Graphics/Shader.h"
+#include "Collision.h"
+
+class Stage
+{
+public:
+    Stage() {}
+    virtual ~Stage() {}
+    virtual void Update(float elapsedTime) = 0;
+    virtual void Render(ID3D11DeviceContext* dc, Shader* shader) = 0;
+    virtual bool RayCast(const DirectX::XMFLOAT3& start, const DirectX::XMFLOAT3& end, HitResult& hit) = 0;
+
+protected:
+    DirectX::XMFLOAT3   position[10] = {};
+    DirectX::XMFLOAT3   angle[10] = {};
+    DirectX::XMFLOAT3   scale[10] = {};
+    DirectX::XMFLOAT4X4 transform[10];
+
+};
