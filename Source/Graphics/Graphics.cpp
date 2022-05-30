eabd8cae30b2e14fdab9aa58d2092ba843275931
kurama13717 <2200460@ecccomp.com>
2022-05-30 15:47:00 +0900

    o

--- Source/Graphics/Graphics.cpp
+++ Source/Graphics/Graphics.cpp
@@ -14,8 +14,10 @@ Graphics::Graphics(HWND hWnd)
 	// 画面のサイズを取得する。
 	RECT rc;
 	GetClientRect(hWnd, &rc);
-	UINT screenWidth = rc.right - rc.left;
-	UINT screenHeight = rc.bottom - rc.top;
+	//UINT screenWidth = rc.right - rc.left;
+	//UINT screenHeight = rc.bottom - rc.top;
+	UINT screenWidth = 1600;
+	UINT screenHeight = 900;
 
 	this->screenWidth = static_cast<float>(screenWidth);
 	this->screenHeight = static_cast<float>(screenHeight);
